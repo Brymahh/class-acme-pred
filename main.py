4535ab6446cfcb323d73c6f644f602f99ba30825
Brymahh <eseoghenabraimah@gmail.com>
2025-06-09 14:15:10 +0100

    data-ingestion finish

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 from src.utils import logger
-# from src import logger
-
-logger.info("This is a test for custom logs")
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
 
 
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj= DataIngestionTrainPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=========")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 
 
-# create ml functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from src.utils import logger
+# # from src import logger
+
+# logger.info("This is a test for custom logs")
+
+
+
+
+
+
+# # create ml functions
